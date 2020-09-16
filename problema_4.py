58967e0d57856341ff174844e3df55ed575ec17c
BabalauCristian10C <70657828+BabalauCristian10C@users.noreply.github.com>
2020-09-16 19:03:18 +0800

    Update problema_4.py

--- problema_4.py
+++ problema_4.py
@@ -1,8 +1,25 @@
-print("introduceti numarul natural n", end =" ")
+print("introduceti numarul natural n", end=" ")
 n = int(input())
 
+
 def m_cm(a):
-    print("metrii in centimetrii = ", a * 100) 
+    print(a, "metrii in centimetrii ", a * 100, sep=" = ", end="\n")
 
 
-print(m_cm(n))
+def k_mg(a):
+    print(a, "kilograme in miligrame ", a * 1000000, sep=" = ", end="\n")
+
+
+def an_zile(a):
+    luni = a * 12
+    saptamani = a * 53
+    zile = a * 365
+    print("numarul de luni", luni, "numarul de zile", zile, " numarul de luni", luni, sep=" = ", end="\n")
+
+
+print \
+        (
+        m_cm(n),
+        k_mg(n),
+        an_zile(n)
+    )
