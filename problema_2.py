041d6fa245b6a8d16a6b6be35266d9cd66bea32d
BabalauCristian10C <70657828+BabalauCristian10C@users.noreply.github.com>
2020-09-16 10:23:44 +0300

    Add files via upload

---
+++ problema_2.py
@@ -0,0 +1,6 @@
+print("intorduceti 1 numar ",end=" ")
+a = int(input())
+print("intorduceti 2 numar ",end=" ")
+b = int(input())
+
+print("summa", a+b, "diferenta", a-b, "catul impartirii", a//b, "restul impartirii", a%b, "ridicarea a la puterea b", a**b, "produsul", a*b, sep=" = ")
