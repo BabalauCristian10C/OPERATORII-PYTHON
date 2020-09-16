f714b9c54d9c567b7b5cc6714a2c19b0918fe0ff
BabalauCristian10C <70657828+BabalauCristian10C@users.noreply.github.com>
2020-09-16 14:18:33 +0300

    Add files via upload

---
+++ problema_5.py
@@ -0,0 +1,12 @@
+x1 = True
+y1 = True
+x2 = False
+y2 = False
+
+print("Tabelul Boolean ", end = "\n \n")
+
+print(x1 and y1,x1 and y2, x2 and y1, x2 and y2)
+print(x1 or y1,x1 or y2, x2 or y1, x2 or y2)
+print( not x1, not y1, not x2, not y2)
+
+
