9893cc188205398114917f689c7a5d0bd63e7201
BabalauCristian10C <70657828+BabalauCristian10C@users.noreply.github.com>
2020-09-16 10:30:51 +0300

    Update problema_3.py

--- problema_3.py
+++ problema_3.py
@@ -2,4 +2,4 @@ print("introduceti lungimea unei laturi", end =" ")
 lat = int(input())
 
 ar = lat**2
-print("volumul", lat**3," aria suprafetei",ar , "aria totala", 6 * ar,sep=" = ")3
+print("volumul", lat**3," aria suprafetei",ar , "aria totala", 6 * ar,sep=" = ")
